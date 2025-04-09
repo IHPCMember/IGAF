df83d4503dc3f5b46d8072e4c3ab2088d88e7b00
IHPCMember <gr0731hs@ed.ritsumei.ac.jp>
2025-04-09 15:03:25 +0900

    Add files via upload

---
+++ set_logger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import logging
+
+def set_logger1(log_path='./logs/test_result_visa_wide_resnet50_2.log'):
+    """Set the logger to log info in terminal and file `log_path`.
+    In general, it is useful to have a logger so that every output to the
+    terminal is saved in a permanent file.
+    Example:
+    ```
+    logging.info("Starting training...")
+    ```
+    Args:
+        log_path: (string) where to log
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:'
+                                                    ' %(message)s'))
+        logger.addHandler(file_handler)
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
